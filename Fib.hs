81507f50494a520f4f63cb41529babdab86db5c1
João Vilhena <joao.mascarenhas2000@gmail.com>
2021-11-24 12:25:48 +0000

    added error treatment for when the argument is a negative number

--- Fib.hs
+++ Fib.hs
@@ -5,21 +5,26 @@ import BigNumber
 fibRec :: (Integral a) => a -> a
 fibRec 0 = 0
 fibRec 1 = 1
-fibRec n = fibRec(n-1) + fibRec(n-2)
+fibRec n   | n < 0 = error "negative numbers not accepted"
+           | otherwise = fibRec(n-1) + fibRec(n-2)
 
 fibLista :: (Integral a) => a -> a
-fibLista n = lista !! fromIntegral(n)
-            where lista = 0 : 1 : map(\x -> lista !! (x - 1) + lista !! (x - 2)) [2..fromIntegral(n)]
+fibLista n   | n < 0 = error "negative numbers not accepted"
+             |otherwise = lista !! fromIntegral(n)
+			  where lista = 0 : 1 : map(\x -> lista !! (x - 1) + lista !! (x - 2)) [2..fromIntegral(n)]
+
 
 fibListaInfinita :: (Integral a) => a -> a
-fibListaInfinita n = listainf !! fromIntegral(n)
-                     where listainf = map fst (iterate(\(x,y) -> (y,x+y)) (0,1))
+fibListaInfinita n   | n < 0 = error "negative numbers not accepted"
+					 | otherwise = listainf !! fromIntegral(n)
+					   where listainf = map fst (iterate(\(x,y) -> (y,x+y)) (0,1))
 
 
 fibRecBN :: BigNumber -> BigNumber
 fibRecBN (False,[0]) = (False,[0])
 fibRecBN (False,[1]) = (False,[1])
-fibRecBN bign = somaBN (fibRecBN(subBN bign (scanner "1"))) (fibRecBN(subBN bign (scanner "2")))
+fibRecBN bign   | fst bign = error "negative numbers not accepted"
+                | otherwise = somaBN (fibRecBN(subBN bign (scanner "1"))) (fibRecBN(subBN bign (scanner "2")))
 
 
 nthBN :: [BigNumber] -> BigNumber -> BigNumber
@@ -32,15 +37,17 @@ createList :: BigNumber  -> BigNumber  -> [BigNumber ]
 createList cur max
         | isGreaterThan abs2 abs1 = cur :createList (somaBN cur (scanner "1")) max
         | otherwise = [max]
-        where abs1 = snd cur
-              abs2 = snd max
+		where abs1 = snd cur
+		      abs2 = snd max
 
 fibListaBN :: BigNumber -> BigNumber
-fibListaBN bign = nthBN lista bign
-                        where aux = createList (scanner "2") bign
-                              lista = scanner "0" : scanner "1" : map(\x -> somaBN (nthBN lista (subBN x (scanner "1"))) (nthBN lista (subBN x (scanner "2")))) aux
+fibListaBN bign  | fst bign = error "negative numbers not accepted"
+                 |otherwise = nthBN lista bign
+                 where aux = createList (scanner "2") bign
+                       lista = scanner "0" : scanner "1" : map(\x -> somaBN (nthBN lista (subBN x (scanner "1"))) (nthBN lista (subBN x (scanner "2")))) aux
 
 
 fibListaInfinitaBN :: BigNumber -> BigNumber
-fibListaInfinitaBN bign = nthBN listainfbn bign
-                          where listainfbn = [x | x <- map fst (iterate(\(x,y) -> (y,somaBN x y)) ((False,[0]),(False,[1])))]
+fibListaInfinitaBN bign   | fst bign = error "negative numbers not accepted"
+                          |otherwise = nthBN listainfbn bign
+                           where listainfbn = [x | x <- map fst (iterate(\(x,y) -> (y,somaBN x y)) ((False,[0]),(False,[1])))]
